bb5f2d4ea385a82be37c543f0813e89daf6329ca
Kent Adriane Goc-ong <kentadriane2@gmail.com>
2025-04-04 14:20:52 +0800

    fix import statement to match file naming convention

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import App from './App.jsx';
+import App from './app.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import { BrowserRouter } from 'react-router-dom';
